44b990a62ba8401945504435120da83df9c9f9ad
kenjikashikoi <bigj1409@gmail.com>
2026-01-20 11:31:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function togglePhase(id) {
+    const phase = document.getElementById(id);
+    phase.style.display = (phase.style.display === "block") ? "none" : "block";
+}
+
+function toggleExercise(id) {
+    const exercise = document.getElementById(id);
+    exercise.style.display = (exercise.style.display === "block") ? "none" : "block";
+}
